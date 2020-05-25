99c14400f73c5459a72114d05350105c893976cf
armin.s98 <arminsoltan1377@gmail.com>
2020-05-25 19:07:55 +0430

    Change data structure of synapse

--- synapse/synapse.py
+++ synapse/synapse.py
@@ -22,35 +22,37 @@ class Synapse:
 
     def connect(self, pre_synaptic_neuron, post_synaptic_neuron):
         if pre_synaptic_neuron not in self.adjacency.keys():
-            self.adjacency[pre_synaptic_neuron] = list()
-        self.adjacency[pre_synaptic_neuron].append([post_synaptic_neuron, 0])
-        if post_synaptic_neuron not in self.reverse_adjacency.keys():
-            self.reverse_adjacency[post_synaptic_neuron] = list()
-        self.reverse_adjacency[post_synaptic_neuron].append([pre_synaptic_neuron, 0])
-
-    def update_weight(self, current_time):
-        c_corr_11 = self.landa * (self.maximum_weight - self.weight)
-        # dw_per_dt = c_corr_11 * self.pre.voltage[current_time] * self.post[-1].voltage[current_time]
-        # self.weight = dw_per_dt * self.dt
+            self.adjacency[pre_synaptic_neuron] = dict()
+        self.adjacency[pre_synaptic_neuron][post_synaptic_neuron] = 0.4
 
     def stdp(self, neuron):
         if neuron in self.adjacency.keys():
             maximum_post_synaptic_time = 0
-            for post_synaptic in self.adjacency[neuron]:
-                maximum_post_synaptic_time = post_synaptic[0].last_spike_time
+            for post_synaptic_neuron in self.adjacency[neuron].keys():
+                if maximum_post_synaptic_time <= post_synaptic_neuron.last_spike_time:
+                    maximum_post_synaptic_time = post_synaptic_neuron.last_spike_time
             delta_t = maximum_post_synaptic_time - neuron.last_spike_time
-            self.add_delta_t_delta_w(neuron, delta_t)
+            delta_w = self._add_delta_t_delta_w(neuron, delta_t)
+            for i in range(len(self.adjacency[neuron])):
+                self.adjacency[neuron][i][1] = max(self.adjacency[neuron][i][1] + delta_w, 0)
         elif neuron in self.reverse_adjacency:
             maximum_pre_synaptic_time = 0
-            for pre_synaptic_neuron in self.reverse_adjacency[neuron]:
-                maximum_pre_synaptic_time = pre_synaptic_neuron[0].last_spike_time
+            pre_synaptic_neurons = list()
+            for pre_synaptic_neuron in self.adjacency.keys():
+                if neuron in self.adjacency[pre_synaptic_neuron].keys():
+                    if maximum_pre_synaptic_time <= pre_synaptic_neuron.last_spike_time:
+                        maximum_pre_synaptic_time = pre_synaptic_neuron.last_spike_time
+                    pre_synaptic_neurons.append(pre_synaptic_neuron)
             delta_t = neuron.last_spike_time - maximum_pre_synaptic_time
-            self.add_delta_t_delta_w(neuron, delta_t)
+            delta_w = self._add_delta_t_delta_w(neuron, delta_t)
+            for pre_synaptic_neuron in pre_synaptic_neurons:
+                self.adjacency[pre_synaptic_neuron][neuron] += delta_w
 
-    def add_delta_t_delta_w(self, neuron, delta_t):
+    def _add_delta_t_delta_w(self, neuron, delta_t):
         if delta_t > 0:
             delta_w = self.amplitude_positive * np.exp(-1 * abs(delta_t) / self.tau_positive)
         else:
             delta_w = self.amplitude_negative * np.exp(-1 * abs(delta_t) / self.tau_negative)
         neuron.delta_t.append(delta_t)
         neuron.delta_w.append(delta_w)
+        return delta_w
